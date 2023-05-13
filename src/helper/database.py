621a9fe823b60f455c3829d1498919587bdde552
Peter Weidenbach <weidenba@gmail.com>
2023-05-13 14:05:11 +0200

    database interface init

--- src/helper/database.py
+++ src/helper/database.py
@@ -15,7 +15,44 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+import sys
+
+from sqlalchemy import orm, engine, Column, Integer, Float
 
 
-def initialize_database():
+class Base(orm.DeclarativeBase):
     pass
+
+
+class MeasuringDataSet(Base):
+    timestamp = Column(Integer, primary_key=True)
+    level = Column(Float)
+
+    def __repr__(self) -> str:
+        return 'timestamp: {}; level: {}'.format(self.timestamp, self.level)
+
+
+class MeasuringPoint_1(MeasuringDataSet):
+    __table_name__ = 'MeasuringPoint_1'
+
+
+class MeasuringPoint_2(MeasuringDataSet):
+    __talbe_name__ = 'MeasuringPoint_2'
+
+
+class DbConnection:
+
+    def __init__(self, provider, user=None, password=None, database=None, hostname=None, port=None):
+        self.base = Base
+
+        if provider == 'sqlite':
+            self.engine = engine.create_engine('sqlite://{}'.format(database))
+        elif provider == 'mysql':
+            self.engine = engine.create_engine('mysql://{}:{}@{}:{}/{}'.format(user, password, hostname, port, database))
+        else:
+            sys.exit('databse provider not supported: {}'.format(provider))
+
+        self.session_maker = orm.sessionmaker(bind=self.engine)
+
+    def create_tables(self):
+        self.base.metadata.create_all(self.engine)
